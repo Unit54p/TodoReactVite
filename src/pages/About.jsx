b86d42f6ccdbc2f61c538a6089adb2c8f9bb7891
Surya <suryaarioprrama@gmail.com>
2025-01-18 23:29:50 +0700

    editing About  page prototype

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,8 +3,45 @@ import React from "react";
 function About() {
   return (
     <>
-      <span className="block  mt-3 font-medium text-2xl text-center ">Welcome to the About Page!</span>
-      <div></div>
+      <span className="block  mt-3 font-medium text-2xl text-center ">
+        Welcome to the About Page!
+      </span>
+      {/* 1 */}
+      <div className="layout-x-15">
+        <div className="flex justify-between items-center align-middle">
+          <div>
+            <span className="text-xl font-medium">Front end</span> <br />
+            React, Tailwind, Vite, Html, Css, dan js
+          </div>
+          <div>
+            <img src="src\img\Rectangle 49.png" alt="" />
+          </div>
+        </div>
+      </div>
+      {/* 2 */}
+      <div className="layout-x-15">
+        <div className="flex justify-between items-center align-middle">
+          <div>
+            <img src="src\img\Rectangle 49.png" alt="" />
+          </div>
+          <div>
+            <span className="text-xl font-medium">Front end</span> <br />
+            React, Tailwind, Vite, Html, Css, dan js
+          </div>
+        </div>
+      </div>
+      {/* 3 */}
+      <div className="layout-x-15">
+        <div className="flex justify-between items-center align-middle">
+          <div>
+            <span className="text-xl font-medium">Front end</span> <br />
+            React, Tailwind, Vite, Html, Css, dan js
+          </div>
+          <div>
+            <img src="src\img\Rectangle 49.png" alt="" />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
